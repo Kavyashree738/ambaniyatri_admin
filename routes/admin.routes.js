f24bc38c70e4c14a9300d28a79d163d2ca6b9d24
Kavyashree738 <jioyattri@gmail.com>
2026-02-14 11:15:46 +0530

    Add saveAdminToken route to admin routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getDriversOnRide,getRideDetails } = require("../controllers/admin.controller");
+const { getDriversOnRide,getRideDetails ,saveAdminToken} = require("../controllers/admin.controller");
 
 // GET /api/admin/drivers/on-ride
 router.get("/drivers/on-ride", getDriversOnRide);
 router.get("/ride/:rideId", getRideDetails);
+router.post("/save-token", saveAdminToken);
 
 module.exports = router;
+
